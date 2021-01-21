919a634b505394b9f985454bda513c177101172e
chris <chris@kpcard.co.kr>
2021-01-21 09:27:32 +0900

    initialize spring boot project

---
+++ backend/src/test/java/kr/co/kpcard/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kpcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
